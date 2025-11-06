3287260c442bdddcc855bdc82c98c8a677c5afa3
Xing gao <738234123@qq.com>
2025-11-06 00:09:06 -0500

    webGL

---
+++ react-pixi/src/components/Experience/Experience.tsx
@@ -0,0 +1,11 @@
+import { Stage } from '@pixi/react'
+
+export const Experience = () => {
+  return (
+    <Stage width={500} height={500}>
+      {/* Experience content goes here */}
+
+    </Stage>
+  )
+}
+
